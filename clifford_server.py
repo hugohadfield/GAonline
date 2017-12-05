b2c7239982f9ad3397e7c2bcee399d9968ee8c59
Hugo Hadfield <hadfield.hugo@gmail.com>
2017-12-05 15:25:59 +0000

    planes and spheres supported

--- clifford_server.py
+++ clifford_server.py
@@ -31,6 +31,15 @@ def get_radius_from_sphere(sphere):
     dual_sphere = sphere*I5
     return math.sqrt( abs(dual_sphere*dual_sphere) )
 
+def get_plane_origin_distance(plane):
+    return float(((plane*I5)|no)[0])
+
+def get_plane_normal(plane):
+    return (plane*I5 - get_plane_origin_distance(plane)*ninf)
+
+def get_nearest_plane_point(plane):
+    return get_plane_normal(plane)*get_plane_origin_distance
+
 @app.route("/")
 def render_main():
     return render_template('main.html')
@@ -42,10 +51,23 @@ def to_sphere():
     print('Recieved blade values: ',present_blades_dict)
     sphere = dict_to_multivector(present_blades_dict)
     print('Sphere: ',sphere)
-    GAcentre = get_center_from_sphere(sphere)
-    print(GAcentre)
+    GAcentre = down(get_center_from_sphere(sphere))
     centre = [GAcentre[1],GAcentre[2],GAcentre[3]]
     radius = get_radius_from_sphere(sphere)
     print('Centre: ',centre)
     print('Radius: ',radius)
     return jsonify(centre=centre,radius=radius) 
+
+@app.route("/to_plane/",methods=['POST'])
+def to_plane():
+    print('RECIEVING PLANE')
+    present_blades_dict = json.loads(request.form.get('present_blades'))
+    print('Recieved blade values: ',present_blades_dict)
+    plane = dict_to_multivector(present_blades_dict)
+    print('Plane: ',plane)
+    normalGA = get_plane_normal(plane)
+    normal = [normalGA[1],normalGA[2],normalGA[3]]
+    distance = get_plane_origin_distance(plane)
+    print('Distance: ', distance)
+    print('Normal: ', normal)
+    return jsonify(distance=distance,normal=normal) 
