145dcf159c410c1d75dd1fc908278d10d906861b
hugohadfield <hadfield.hugo@gmail.com>
2019-01-02 18:29:05 +0000

    Update GAOnline.js

--- static/GAOnline.js
+++ static/GAOnline.js
@@ -533,8 +533,8 @@ function resetScene(scene, floor_and_axes_bool){
     // Remove everything from the scene first
     for (let i = scene.children.length - 1; i >= 0; i--) {
         var mesh = scene.children[i];
-        mesh.geometry.dispose();
         scene.remove(mesh);
+        mesh = null;
     }
     if (floor_and_axes_bool){
         // Build coordinate axes
